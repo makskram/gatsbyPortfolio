012f42d243a7cef223047665a2b2d9e0d2cc1081
Maksim Kramer <65328705+makskram@users.noreply.github.com>
2021-12-03 00:09:13 -0500

    Update GithubButtons.jsx

--- src/components/GithubButtons/GithubButtons.jsx
+++ src/components/GithubButtons/GithubButtons.jsx
@@ -5,21 +5,21 @@ const GithubButton = () => (
   <>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio/fork"
+      href="https://github.com/makskram/gatsbyPortfolio/fork"
       data-icon="octicon-repo-forked"
       data-size="large"
       data-show-count="true"
-      aria-label="Fork cobidev/gatsby-simplefolio on GitHub"
+      aria-label="Fork makskram/gatsbyPortfolio on GitHub"
     >
       Fork
     </GitHubButton>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio"
+      href="https://github.com/makskram/gatsbyPortfolio"
       data-icon="octicon-star"
       data-size="large"
       data-show-count="true"
-      aria-label="Star cobidev/gatsby-simplefolio on GitHub"
+      aria-label="Star makskram/gatsbyPortfolio on GitHub"
     >
       Star
     </GitHubButton>
